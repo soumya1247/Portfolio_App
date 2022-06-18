f73ba9f6b90dfdeb13d245751a79aebfc500bbad
soumya1247 <soumyadexter7@gmail.com>
2022-06-18 10:42:07 +0530

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BsTwitter, BsLinkedin } from 'react-icons/bs'
+
+function SocialMedia() {
+        return (
+            <div className='app__social'>
+                <div>
+                    <BsTwitter />
+                </div>
+                <div>
+                    <BsLinkedin />
+                </div>
+            </div>
+        )
+    }
+
+export default SocialMedia
